10e249abcc65d45e371022e54bc903df08941f95
agkura <agkura.code@gmail.com>
2017-04-27 01:51:52 -0700

    Add I/O practice

---
+++ w5d3/callbacks.js
@@ -0,0 +1,40 @@
+// window.setTimeout(()=>alert('Hammertime!'), 5000)
+//
+// function hammerTime(time) {
+//   window.setTimeout(()=>`${time} is hammertime`, time)
+// }
+
+
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function teaTime(){
+  let first, second;
+
+  reader.question('Would you like some tea?', (res) => {
+    first = res;
+    console.log(`You replied ${res}.`);
+    another();
+  });
+  
+  function another(){
+    reader.question('Would you like some biscuits?', (res) => {
+      second = res;
+      console.log(`You replied ${res}.`);
+      complete();
+    });
+  }
+
+  function complete(){
+    const firstRes = (first === 'yes') ? 'do' : 'don\'t';
+    const secondRes = (second === 'yes') ? 'do' : 'don\'t';
+    console.log(`So you ${firstRes} want tea and you ${secondRes} want biscuits.`);
+    reader.close();
+  }
+}
+
+teaTime()
