a174d78dc20726b1db2b41df7453d347a8f6651d
agkura <agkura.code@gmail.com>
2017-04-30 00:57:08 -0700

    Add wacky jquery

--- w5d4/canvas_practice/practice.js
+++ w5d4/canvas_practice/practice.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(){
+  var canvas = document.getElementById('mycanvas');
+  var ctx = canvas.getContext('2d');
+    ctx.fillRect(0,0,500,500);
+    ctx.fillStyle = "black";
 
+    ctx.beginPath();
+    ctx.arc(150,300,50,0,2 * Math.PI);
+    ctx.lineWidth = 1;
+    ctx.fillStyle = "white";
+    ctx.fill();
+    ctx.stroke();
 });
