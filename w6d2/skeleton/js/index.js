4181063525732c0077d5ef9551839454f52fd75f
agkura <agkura.code@gmail.com>
2017-05-02 22:21:40 -0700

    Remove undefined logs

--- w6d2/skeleton/js/index.js
+++ w6d2/skeleton/js/index.js
@@ -1,8 +1,7 @@
 console.log("Hello from the JavaScript console!");
 
-const weather = $.ajax({type:'GET', url: "http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=bcb83c4b54aee8418983c2aff3073b3b", success(data){console.log(data);}})
+$.ajax({type:'GET', url: "http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=bcb83c4b54aee8418983c2aff3073b3b", success(data){console.log(data);}})
 // Your AJAX request here
 console.log("after ajax request");
-console.log(weather[weather].description);
 
 // Add another console log here, outside your AJAX request
