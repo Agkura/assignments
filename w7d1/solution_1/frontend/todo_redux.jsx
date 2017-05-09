687e21e62a4a24f743f65f2a014de2882ce69347
agkura <agkura.code@gmail.com>
2017-05-08 22:48:35 -0700

    Add custom middleware logger

--- w7d1/solution_1/frontend/todo_redux.jsx
+++ w7d1/solution_1/frontend/todo_redux.jsx
@@ -8,7 +8,25 @@ document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
   const store = configureStore(preloadedState);
+  const newStore = applyMiddlewares(store, addLoggingToDispatch);
 
   const root = document.getElementById('content');
-  ReactDOM.render(<Root store={store} />, root);
+  ReactDOM.render(<Root store={newStore} />, root);
 });
+
+const addLoggingToDispatch = store => next => action =>{
+  let qdispatch = store.dispatch;
+  console.log(store.getState());
+  console.log(action);
+  let result = qdispatch(action);
+  console.log(store.getState());
+  return result;
+}
+
+const applyMiddlewares = (store, ...middleware) => {
+  let dispatch = store.dispatch;
+  middleware.forEach((mw) => {
+    dispatch = mw(store)(dispatch);
+  });
+  return Object.assign({}, store, { dispatch });
+};
