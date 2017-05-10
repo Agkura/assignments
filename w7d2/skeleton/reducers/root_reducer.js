fc37529f1bc96488898a65934a8013ae9b6f1736
agkura <agkura.code@gmail.com>
2017-05-09 22:09:03 -0700

    Create Store

--- w7d2/skeleton/reducers/root_reducer.js
+++ w7d2/skeleton/reducers/root_reducer.js
@@ -1,3 +1,9 @@
 import { combineReducers } from 'redux';
 
 import GiphysReducer from './giphys_reducer';
+
+const RootReducer =  combineReducers({
+  giphsy: GiphysReducer
+});
+
+export default RootReducer;
